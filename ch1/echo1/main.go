3959780bee2ec866d96bb12ad543a83c36b30abd
sunruilong <sunlong0724@sina.com>
2020-09-30 10:09:33 +0800

    start learning go.

---
+++ ch1/echo1/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	i := 1
+	var j = i++
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
+
+func main0() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
---
+++ ch2/boiling.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const boilingF = 212.0
+
+func main() {
+	var f = boilingF         // aaaa
+	var c = (f - 32) * 5 / 9 // bbbb
+
+	var a int
+	var b int
+	fmt.Fprintln(os.Stdout, &a, &b)
+
+	fmt.Printf("boiling point = %gF or %gC\n", f, c)
+
+}
