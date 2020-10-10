daeab8cdd7e8f082187a373e49279d7cb2ec886b
sunruilong <sunlong0724@sina.com>
2020-10-10 13:43:19 +0800

    add test defer

--- test_defer/test_defer.go
+++ test_defer/test_defer.go
@@ -10,6 +10,29 @@ func test() (z int) {
 	return 100
 }
 
-func main() {
+func main0() {
 	fmt.Println("test:", test())
+
+	const (
+		i = 1 << iota
+		j
+		k
+		l
+	)
+
+	fmt.Println("i=", i)
+	fmt.Println("j=", j)
+	fmt.Println("k=", k)
+	fmt.Println("l=", l)
+}
+
+func main() {
+	fmt.Println("main start")
+	ch := make(chan string)
+	go func() {
+		val := <-ch // 出 chan
+		fmt.Println(val)
+	}()
+	ch <- "a" // 入 chan
+	fmt.Println("main end")
 }
